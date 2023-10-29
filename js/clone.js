49dcc2112310911201cc19f23dae84624d5f074e
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2023-10-29 22:53:48 +0100

    Sonntag, 2023-10-29 (22:53:46)

--- js/clone.js
+++ js/clone.js
@@ -1,30 +1,31 @@
 // 
 // Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
-// v0.2.1
+// v0.2.2
 // 
 // Just a tiny function to *really* clone objects (etc.); .. with all types, not only JSON supported ones
 // or so (sometimes the web referes to just `JSON.parse(JSON.stringify({}))`);
 // 
 // If you really start with an own `Map` instance (2nd parameter), you can even define to replace any
 // occurence of some object with your own values.
-// Functions are also cloned, if not native ones, including all of their members, if any additional exist.
+// Functions are also cloned, if not native ones or `_function == false` (including all of their members,
+// if any additional are defined under 'em).
 //
 
 //
-Reflect.defineProperty(Reflect, 'clone', { value: (_object, _map = null) => {
+Reflect.defineProperty(Reflect, 'clone', { value: (_object, _map = null, _function = false) => {
 	if(!_map) _map = new Map(); else if(_map.has(_object)) return _map.get(_object); else if(!Reflect.isExtensible(_object)) return _object;
 	else if(typeof _object === 'undefined' || _object === null) return _object; const keys = Reflect.ownKeys(_object);
 	const isArray = (Array._isArray(_object) ? _object.length : -1); var result; if(isArray > -1) { result = new Array(isArray);
-	for(var i = 0; i < _object.length; ++i) result[i] = Reflect.clone(_object[i], _map); for(var i = _object.length - 1; i >= 0; --i)
-	keys.remove(i.toString()); keys.remove('length'); } else if(typeof _object === 'function') { if(Function.isNative(_object))
+	for(var i = 0; i < _object.length; ++i) result[i] = Reflect.clone(_object[i], _map, _function); for(var i = _object.length - 1; i >= 0; --i)
+	keys.remove(i.toString()); keys.remove('length'); } else if(typeof _object === 'function') { if(Function.isNative(_object) || !_function)
 	result = _object; else try { eval('result = ' + _object.toString()); } catch(_error) { result = _object; } keys.remove(
 		'length', 'name', 'arguments', 'caller', 'prototype'); } else if(Object.isNull(_object)) result = Object.create(null);
 	else try { result = Object.create(Reflect.getPrototypeOf(_object)); } catch(_error) { result = {}; }
 	_map.set(_object, result); _map.set(result, result); var desc; for(var i = 0; i < keys.length; ++i) {
 	try { desc = Reflect.getOwnPropertyDescriptor(_object, keys[i]); } catch(_err) { desc = { value: _object[keys[i]] }; }
-		if('value' in desc) desc.value = Reflect.clone(desc.value, _map);
-		else { if('get' in desc) desc.get = Reflect.clone(desc.get, _map);
-			if('set' in desc) desc.set = Reflect.clone(desc.set, _map); }
+		if('value' in desc) desc.value = Reflect.clone(desc.value, _map, _function);
+		else { if('get' in desc) desc.get = Reflect.clone(desc.get, _map, _function);
+			if('set' in desc) desc.set = Reflect.clone(desc.set, _map, _function); }
 	Reflect.defineProperty(result, keys[i], desc); } return result;
 }});
 
