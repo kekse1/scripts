845bce532b7c0395ba215920cedb8e9d9eca5904
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-01-09 05:20:06 +0100

    Dienstag, 2024-01-09 (05:20:04)

--- js/clone.js
+++ js/clone.js
@@ -1,6 +1,6 @@
 // 
 // Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
-// v0.3.1
+// v0.3.2
 // 
 // Just a tiny function to *really* clone objects (etc.); .. with all types, not only JSON supported ones
 // or so (sometimes the web referes to just `JSON.parse(JSON.stringify({}))`);
@@ -18,7 +18,7 @@ Reflect.defineProperty(Reflect, 'clone', { value: (_object, _map = null, _functi
 	if(!_map) _map = new Map(); else if(_map.has(_object)) return _map.get(_object); else if(!Reflect.isExtensible(_object)) return _object;
 	else if(typeof _object === 'undefined' || _object === null) return _object; const keys = Reflect.ownKeys(_object);
 	var cloneFunc; if(typeof _object.clone === 'function') cloneFunc = _object.clone.bind(_object, ... _clone_args); else if(typeof _object.cloneNode === 'function')
-		cloneFunc = _object.cloneNode.bind(_object, true, ... _clone_args); else cloneFunc = null; const isArray = (cloneFunc === null ? -1 : (Array._isArray(_object) ?
+		cloneFunc = _object.cloneNode.bind(_object, true, ... _clone_args); else cloneFunc = null; const isArray = (cloneFunc !== null ? -1 : (Array._isArray(_object) ?
 			_object.length : -1)); var result; if(cloneFunc !== null) { result = cloneFunc(); _map.set(_object, result); return result; }
 	else if(isArray > -1) { result = new Array(isArray); for(var i = 0; i < _object.length; ++i) { keys.remove(i.toString()); result[i] = Reflect.clone(_object[i], _map, _function,
 		... _clone_args); keys.remove('length'); }} else if(typeof _object === 'function') { if(Function.isNative(_object) || !_function) result = _object;
