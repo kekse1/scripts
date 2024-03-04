7f892ba4e2a729a5cdd85a0950721045f3accd4b
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-03-04 05:21:48 +0100

    Montag, 2024-03-04 (05:21:46)

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 //
+//is TODO atm./!!
+//
+
+//
 // Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
 // https://kekse.biz/
-// v0.3.0
+// v0.4.0
 //
 // Helper script for my v4 project @ https://github.com/kekse1/v4/.
 //
@@ -20,14 +24,14 @@
 // file paths these are used, so we're using my `console.confirm()` before
 // writing to any file..
 //
-// It's currently under development, so stay tuned (currently working on
-// TWO projects, this night, next day, ...)! ;-)
-//
 
 //
 const PATH_SUB = [ 'lib', 'web' ];
 var PATH = '../js/';
 var PATH_INDEX, PATH_SUMMARY;
+
+const DEFAULT_MODE_DIR = 0o700;
+const DEFAULT_MODE_FILE = 0o600;
 
 //
 var withFiles = (process.argv.length > 2);
@@ -45,6 +49,53 @@ ready(() => {
 		{
 			INDEX = path.resolve(INDEX);
 			SUMMARY = path.resolve(SUMMARY);
+
+			const dirIndex = path.dirname(INDEX);
+			const dirSummary = path.dirname(SUMMARY);
+
+			if(fs.exists(dirIndex))
+			{
+				if(!fs.exists.directory(dirIndex))
+				{
+					console.error('Path to index output file exists, but ain\'t a directory!');
+					process.exit(2);
+				}
+			}
+			else
+			{
+				fs.mkdirSync(dirIndex, { recursive: true, mode: DEFAULT_MODE_DIR });
+			}
+
+			if(fs.exists(dirSummary))
+			{
+				if(!fs.exists.directory(dirSummary))
+				{
+					console.error('Path to summary output file exists, but ain\'t a directory!');
+					process.exit(3);
+				}
+			}
+			else
+			{
+				fs.mkdirSync(dirSummary, { recursive: true, mode: DEFAULT_MODE_FILE });
+			}
+
+			if(fs.exists(INDEX))
+			{
+				if(!fs.exists.file(INDEX))
+				{
+					console.error('Index output file path exists, but ain\'t a regular file!');
+					process.exit(4);
+				}
+			}
+
+			if(fs.exists(SUMMARY))
+			{
+				if(!fs.exists.file(SUMMARY))
+				{
+					console.error('Summary output file path exists, but ain\'t a regular file!');
+					process.exit(5);
+				}
+			}
 		}
 		else
 		{
@@ -54,7 +105,7 @@ ready(() => {
 	}
 
 	//
-	console.info('Will create .json output for JavaScript code of % directories.', PATH_SUB.length);
+	console.info('Creates .json output for JavaScript code of % directories.', PATH_SUB.length);
 	console.log();
 
 	//
@@ -76,24 +127,24 @@ ready(() => {
 		console.info('Summary: ' + SUMMARY.quote());
 		console.log();
 
-		console.confirm(proceed);
+		console.confirm(proceed, 'Do you really want to continue (and WRITE DATA to two files) [Yes|No]? ');
 	}
 	else proceed(null);
 });
 
 //
 const proceed = (_bool = null, _answer) => { if(_bool === false) { console.log('Goodbye!'); process.exit(); }
-	var amount = (PATH_SUB.length * 2); const cb = () => { if(--amount <= 0) return handle(result); };
+	var amount = 0; const cb = () => { if(--amount <= 0) return handle(result); };
 	const result = Object.create(null); for(const sub of PATH_SUB) fs.readdir(sub, {
-		encoding: 'utf8', withFileTypes: true, recursive: true }, (_err, _files) => {
-			if(_err) return error(_err); for(var i = 0; i < _files.length; ++i) {
-				if(_files[i].name[0] !== '.' && /*_files[i].isFile() &&*/ _files[i].name.endsWith('.js')) {
-					const p = path.join(_files[i].path, _files[i].name); result[p] = {
-						base: _files[i].name, name: path.join(_files[i].path, _files[i].name)
+		encoding: 'utf8', withFileTypes: true, recursive: true }, (_err, _files) => { if(_err) return error(_err);
+			for(var i = 0; i < _files.length; ++i) { if(_files[i].name[0] !== '.' && /*_files[i].isFile() &&*/ _files[i].name.endsWith('.js')) {
+				++amount; const p = path.join(_files[i].path, _files[i].name); result[p] = {
+					base: path.basename(_files[i].name, '.js'),
+						name: path.join(_files[i].path, _files[i].name)
 							.split(path.sep).slice(-2).join(path.sep) };
-					fs.stat(p, { bigint: false }, (_err, _stats) => {
-						if(_err) return error(_err); else cb();
-						result[p].size = _stats.size; }); }} cb(); }); };
+				fs.stat(p, { bigint: false }, (_err, _stats) => {
+					if(_err) return error(_err); else cb();
+					result[p].size = Math.size.render(result[p].bytes = _stats.size).toString(); }); }}}); };
 
 const handle = (_result) => {
 	var rest = Object.keys(_result).length; const cb = (_path, _full, _real) => {
@@ -110,11 +161,23 @@ const countLines = (_path, _callback) => { var full = 0, real = 0, last;
 //
 const prepare = (_result) => { const keys = Object.keys(_result);
 	const result = new Array(keys.length); for(var i = 0; i < keys.length; ++i)
-		result[i] = _result[keys[i]]; return finish(result); };
+		result[i] = _result[keys[i]]; result.sort('bytes', false); return finish(result); };
+
+const prepareSummary = (_result) => {
+	//TODO/!!
+	return {}; };
 
 //
-const finish = (_result) => {
-	console.dir(_result);
+const finish = (_result) => { //index(_result); summary(_result);
+console.dir(_result);
+	if(!withFiles) console.debug(4, 'Success with % files!', _result.length); };
+
+const index = (_result) => { if(!withFiles) return process.stdout.write(JSON.stringify(_result));
+	//TODO/write file(s)!
+};
+
+const summary = (_result) => { if(!withFiles) return process.stderr.write(JSON.stringify(prepareSummary(_result)));
+	//TODO/!!write file(s)!!
 };
 
 //
