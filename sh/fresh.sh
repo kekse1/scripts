407692bf36ffd5a7cc2dae5582f5f49d32ec4157
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-03-20 01:29:44 +0100

    [1710894582952240178] updated fresh.sh to v0.2.0

--- sh/fresh.sh
+++ sh/fresh.sh
@@ -1,19 +1,25 @@
 #
 # Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
 # https://kekse.biz/ https://github.com/kekse1/scripts/
-# v0.1.4
+# v0.2.0
 #
 # Tiny helper; maybe copy it to /etc/profile.d/fresh.sh
 #
 
-_DATE_FORMAT='%s%N'
+_DATE_FORMAT='%s'
+#_DATE_FORMAT='%s%N'
 #_DATE_FORMAT_WITHOUT='%s%N // %Y-%m-%d / %H:%M:%S'
 
 fresh()
 {
-	if [[ $# -eq 0 ]]; then
+	dir="`git rev-parse --git-dir 2>/dev/null`"
+
+	if [[ $? -ne 0 ]]; then
+		echo " >> Not inside a git repository!" >&2
+		return 1
+	elif [[ $# -eq 0 ]]; then
 		echo " >> Please specify a description for this \`git\` commit!" >&2
-		return
+		return 2
 	fi
 
 	_txt="`date +"$_DATE_FORMAT"`"
