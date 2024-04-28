211a80ce599d87cc78fc94655e12f1b1f4f2a6d4
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-04-28 18:42:19 +0200

    [1714322538] links.js v0.5.0

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Every script is made by myself, arose out of necessity.. (most?) without any dep
 5. [Copyright and License](#copyright-and-license)
 
 ## News
-* \[**2024-04-27**\] Updated [`links.js`](#linksjs) to **v0.4.3**.
+* \[**2024-04-27**\] Updated [`links.js`](#linksjs) to **v0.5.0**.
 * \[**2024-04-23**\] New [`convert-to-gguf.sh`](#convert-to-ggufsh) script, **v0.0.4**
 * \[**2024-04-23**\] Created the [`nightlounge.sh`](#nightloungesh) script, **v0.2.2**
 * \[**2024-04-22**\] Updated [`hugging.sh`](#huggingsh) to **v0.2.4**..
@@ -279,7 +279,7 @@ extensions are being fully cloned. And even the functions themselves (if `_funct
 **no circular dependencies** occure! **;-)**
 
 ### [`links`.js](js/links.js)
-* [Version **v0.4.3**](js/links.js) (updated **2024-04-27**)
+* [Version **v0.5.0**](js/links.js) (updated **2024-04-28**)
 
 This class extracts all links from `.html` files. It should work better than
 regular expressions, since it covers many possible codes. And this class should
@@ -294,7 +294,7 @@ adapted to it (relative links could be a problem otherwise).
 
 Since **v0.4.0** also with `DEFAULT_UNIQUE = true`; extra `.extract()` function
 (original `.onData()` was meant for stream events); plus some improvements and
-bugs fixed.
+bugs fixed. And more **big changes** since **v0.5.0**.
 
 Nice one; have phun.
 
--- js/links.js
+++ js/links.js
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
  * https://kekse.biz/ https://github.com/kekse1/scripts/
- * v0.4.3
+ * v0.5.0
  */
 
 //
@@ -9,14 +9,15 @@ const DEFAULT_ENCODING = 'utf8';
 const DEFAULT_ATTRIBS = [ 'href', 'src' ];
 const DEFAULT_SCHEME = [ 'http:', 'https:' ];
 const DEFAULT_UNIQUE = true;
+const DEFAULT_THROW = true;
 
 //
 class Links
 {
 	constructor(... _args)
 	{
+		var source = undefined;
 		var unique = null;
-		var source = null;
 		var scheme = null;
 
 		for(var i = 0; i < _args.length; ++i)
@@ -40,11 +41,6 @@ class Links
 			}
 		}
 
-		if(typeof source === 'string')
-		{
-			source = new URL(source);
-		}
-
 		if(unique !== null)
 		{
 			this._unique = unique;
@@ -52,11 +48,24 @@ class Links
 
 		if(scheme !== null)
 		{
-			this.scheme = scheme;
+			this._scheme = scheme;
 		}
-		else
+
+		if(typeof source === 'string')
 		{
-			this.scheme = DEFAULT_SCHEME;
+			try
+			{
+				source = new URL(source).href;
+			}
+			catch(_err)
+			{
+				if(DEFAULT_THROW)
+				{
+					throw _err;
+				}
+				
+				source = undefined;
+			}
 		}
 
 		this.source = source;
@@ -64,167 +73,16 @@ class Links
 		this.reset();
 	}
 
-	static filter(_array, _scheme, _source)
+	get scheme()
 	{
-		if(!Array.isArray(_scheme))
-		{
-			_scheme = DEFAULT_SCHEME;
-		}
-		else if(_scheme.length === 0)
-		{
-			return [ ... _array ];
-		}
-		
-		const result = [];
-		var res;
-
-		for(var i = 0, j = 0; i < _array.length; ++i)
-		{
-			if(res = this.url(_array[i], _source))
-			{
-				if(typeof res === 'string')
-				{
-					result[j++] = _array[i];
-				}
-				else if(_scheme.includes(res.protocol))
-				{
-					if(typeof _array[i] === 'string')
-					{
-						res = _array[i];//original form
-					}
-
-					result[j++] = res;
-				}
-			}
-		}
-
-		return result;
+		if(Array.isArray(this._scheme)) return this._scheme;
+		return DEFAULT_SCHEME;
 	}
-
-	static url(_url_array, _source)
+	
+	set scheme(_value)
 	{
-		if(_url_array instanceof URL)
-		{
-			return _url_array;
-		}
-		else if(typeof _url_array === 'string')
-		{
-			if(typeof _source !== 'string' || _source.length === 0)
-			{
-				if(!(_source instanceof URL))
-				{
-					_source = undefined;
-				}
-			}
-
-			try
-			{
-				return new URL(_url_array, _source);
-			}
-			catch(_error)
-			{
-				return _url_array;
-			}
-		}
-		else if(!Array.isArray(_url_array))
-		{
-			return null;
-		}
-
-		const result = [];
-		var res;
-		
-		for(var i = 0, j = 0; i < _url_array.length; ++i)
-		{
-			if(res = this.url(_url_array[i], _source))
-			{
-				result[j++] = res;
-			}
-		}
-
-		return result;
-	}
-
-	static href(_url_array)
-	{
-		if(_url_array instanceof URL)
-		{
-			return _url_array.href;
-		}
-		else if(typeof _url_array === 'string')
-		{
-			return _url_array;
-		}
-		else if(!Array.isArray(_url_array))
-		{
-			return null;
-		}
-
-		const result = [];
-		var res;
-
-		for(var i = 0, j = 0; i < _url_array.length; i++)
-		{
-			if(res = this.href(_url_array[i]))
-			{
-				result[j++] = res;
-			}
-		}
-
-		return result;
-	}
-
-	static unique(_array, _source)
-	{
-		const cmp = (_a, _b, _cast = !!_source) => {
-			if(_cast)
-			{
-				if(typeof _a === 'string')
-					_a = this.url(_a, _source);
-				if(typeof _b === 'string')
-					_b = this.url(_b, _source);
-			}
-
-			if(typeof _a === 'string')
-			{
-				if(typeof _b === 'string')
-				{
-					return (_a === _b);
-				}
-
-				return (_a === _b.href);
-			}
-			else if(typeof _b === 'string')
-			{
-				return (_a.href === _b);
-			}
-
-			return (_a.href === _b.href);
-		};
-
-		const result = [];
-		var inc;
-
-		for(var i = 0, j = 0; i < _array.length; ++i)
-		{
-			inc = false;
-
-			for(const r of result)
-			{
-				if(cmp(_array[i], r))
-				{
-					inc = true;
-					break;
-				}
-			}
-
-			if(!inc)
-			{
-				result[j++] = _array[i];//original form
-			}
-		}
-
-		return result;
+		if(Array.isArray(_value)) return this._scheme = [ ... _value ];
+		return this.scheme;
 	}
 
 	get unique()
@@ -253,6 +111,7 @@ class Links
 		this.openTag = false;
 		this.value = null;
 		this.position = 0;
+		this.fin = false;
 	}
 
 	destroy()
@@ -266,6 +125,13 @@ class Links
 		return DEFAULT_ATTRIBS;
 	}
 
+	finish()
+	{
+		//this.emit('finish', this.links, this);
+		this.fin = true;
+		return this.links;
+	}
+	
 	extract(_data)
 	{
 		var result = this.onData(_data);
@@ -273,6 +139,32 @@ class Links
 		return result;
 	}
 
+	push(_value)
+	{
+		if(!_value) return false; _value = encodeURI(_value);
+		if(this.unique && this.links.includes(_value)) return false;
+		
+		try
+		{
+			_value = new URL(_value, this.source);
+			
+			if(this.scheme.length > 0)
+			{
+				if(!this.scheme.includes(_value.protocol))
+					return false;
+			}
+			
+			_value = _value.href;
+			
+			if(this.unique && this.links.includes(_value))
+				return false;
+		}
+		catch(_err) {}
+
+		this.links.push(_value);
+		return true;
+	}
+	
 	onData(_chunk)
 	{
 		if(_chunk === null)
@@ -285,30 +177,20 @@ class Links
 		var byte, char;
 
 		const atString = (_index, _needle) => {
-			if(_needle.length > (_chunk.length - _index))
-				return false;
-			const cmp = _chunk.substr(_index, _needle.length).toLowerCase();
-			return (cmp === _needle.toLowerCase());
+			return (_chunk.substr(_index, _needle.length).toLowerCase() === _needle.toLowerCase());
 		};
 
 		const atArray = (_index, _needle) => {
-			throw new Error('TODO');
+			var cmp = '', byte; for(var i = _index, j = 0; i < _chunk.length && j < _needle.length; ++i, ++j)
+			{	if((byte = _chunk[i]) >= 65 && byte <= 90) byte += 32;//case IN-sensitive..
+				cmp += String.fromCharCode(byte);	}
+			return (_needle.toLowerCase() === cmp);
 		};
 
 		const at = (_index, _needle) => {
-			if(str) return atString(_index, _needle);
+			if(_needle.length > (_chunk.length - _index)) return false;
+			else if(str) return atString(_index, _needle);
 			return atArray(_index, _needle);
-		};
-
-		const push = (_link) => {
-			if(!_link) return false;
-			_link = encodeURI(_link);
-			
-			if(this.unique && this.links.includes(_link))
-				return false;
-			
-			this.links.push(_link);
-			return true;
 		};
 
 		chunkLoop: for(var i = 0; i < _chunk.length; ++i)
@@ -336,7 +218,7 @@ class Links
 			else if(this.openLink === char)
 			{
 				this.openLink = false;
-				push(this.value);
+				this.push(this.value);
 				this.value = null;
 			}
 			else if(this.value === null)
@@ -387,7 +269,7 @@ class Links
 			{
 				this.openLink = false;
 				this.openTag = false;
-				push(this.value);
+				this.push(this.value);
 				this.value = null;
 			}
 			else
@@ -397,26 +279,6 @@ class Links
 		}
 
 		this.position += _chunk.length;
-		return this.links;
-	}
-
-	finish()
-	{
-		if(this.source)
-		{
-			this.links = Links.url(this.links, this.source);
-		}
-
-		if(this.scheme)
-		{
-			this.links = Lithis.schemefilter(this.links, this.scheme, this.source);
-		}
-
-		if(this.unique)
-		{
-			this.links = Links.unique(this.links);
-		}
-		
 		return this.links;
 	}
 }
