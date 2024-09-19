2b16ce5fc40c4dee876861b825a902270e07e737
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-09-19 05:52:08 +0200

    1726717926976502881

--- sh/baseutils.sh
+++ sh/baseutils.sh
@@ -86,7 +86,7 @@ eol()
 
 line()
 {
-	line="$1"; [[ -z "$line" ]] && line="$LINE"; [[ -z "$line" ]] && line="#"
+	line="$1"; [[ -z "$line" ]] && line="$LINE"; [[ -z "$line" ]] && line="="
 	line="${line::1}"; for i in $(seq 1 `width`); do echo -n "$line"; done
 }
 
