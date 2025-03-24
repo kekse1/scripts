f9b63c2e26614e94c7e0906e5f06336671798079
Sebastian Kucharczyk <kuchen@kekse.biz>
2025-03-24 04:18:43 +0100

    1742786322079829218

--- sh/crypto.sh
+++ sh/crypto.sh
@@ -3,41 +3,53 @@
 # 
 # Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
 # https://kekse.biz/ https://github.com/kekse1/scripts/
-# v0.0.2
+# v0.0.3
 #
 # Manage LUKS encryption via `cryptsetup` (dependency).
 #
-# STILL **TODO**!111
+# My aim is to transparently `cmount` or `cumount`, plus
+# the initial format/preparation of crypto devices/mounts.
+#
+# STILL only **TODO**!!111
 #
 
 #
 __crypto_cryptsetup="cryptsetup"
 __crypto_error_cryptsetup_not_found="The \`cryptsetup\` utility doesn't seem to be installed on your system!"
-__crypto_error_missing_device="Missing \`device\` parameter (a file path, probably a block device)!"
+__crypto_error_invalid_device="Invalid \`device\` parameter, maybe it doesn't exist (a file path, probably a block device)!"
+__crypto_error_invalid_mount="Invalid \`mount point\` parameter; maybe there's no such *directory*!"
 
 #
 cryptoFormat()
 {
 	local CRYPTSETUP="$(which "${__crypto_cryptsetup}" 2>/dev/null)"; if [[ -z "$CRYPTSETUP" ]]; then
 		echo "[ERROR] ${__crypto_error_cryptsetup_not_found}" >&2; return 1; fi
-	local DEVICE="$1"; if [[ -z "$DEVICE" ]]; then
-		echo "[ERROR] ${__crypto_error_missing_device}" >&2; return 2; fi
+	local DEVICE="$1"; if [[ -z "$DEVICE" || ! -e "$DEVICE" ]]; then
+		echo "[ERROR] ${__crypto_error_invalid_device}" >&2; return 2; fi
 	local CMD="${CRYPTSETUP} luksFormat"
 	CMD="${CMD} --type luks2 --cipher aes-xts-plain64 --hash sha512 --key-size 512 --iter-time 3000 --use-urandom --verify-passphrase" # "--pbkdf {argon2id,pbkdf2}"
 	CMD="${CMD} ${DEVICE}"
 }
 
-cryptoStart()
+cmount()
 {
 	local CRYPTSETUP="$(which "${__crypto_cryptsetup}" 2>/dev/null)"; if [[ -z "$CRYPTSETUP" ]]; then
 		echo "[ERROR] ${__crypto_error_cryptsetup_not_found}" >&2; return 1; fi
+	local DEVICE="$1"; if [[ -z "$DEVICE" || ! -e "$DEVICE" ]]; then
+		echo "[ERROR] ${__crypto_error_invalid_device}" >&2; return 2; fi
+	local MOUNT="$2"; if [[ -z "$MOUNT" || ! -d "$MOUNT" ]]; then
+		echo "[ERROR] ${__crypto_error_invalid_mount}" >&2; return 3; fi
 	local CMD="${CRYPTSETUP}"
 }
 
-cryptoStop()
+cumount()
 {
 	local CRYPTSETUP="$(which "${__crypto_cryptsetup}" 2>/dev/null)"; if [[ -z "$CRYPTSETUP" ]]; then
 		echo "[ERROR] ${__crypto_error_cryptsetup_not_found}" >&2; return 1; fi
+	local DEVICE="$1"; if [[ -z "$DEVICE" || ! -e "$DEVICE" ]]; then
+		echo "[ERROR] ${__crypto_error_invalid_device}" >&2; return 2; fi
+	local MOUNT="$2"; if [[ -z "$MOUNT" || ! -d "$MOUNT" ]]; then
+		echo "[ERROR] ${__crypto_error_invalid_mount}" >&2; return 3; fi
 	local CMD="${CRYPTSETUP}"
 }
 
