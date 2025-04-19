1b112de2a73444c07d9e531a474b5403a854d3b8
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2025-04-19 11:25:20 +0200

    1745054719248397144

--- src/reverse.sh
+++ src/reverse.sh
@@ -3,13 +3,13 @@
 #
 # Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
 # https://kekse.biz/ https://github.com/kekse1/scripts/
-# v0.2.0
+# v0.3.0
 #
 # My own solution (instead of using `autossh` or so).
 #
 # Kinda 'watchdog', w/ pause between retries. For some
 # reverse ssh tunnel. Default configuration opens the
-# port (2222) on your remote machine - as a relay/tunnel
+# port (1024) on your remote machine - as a relay/tunnel
 # to your local SSH server (so without port forwarding).
 #
 # Use <Ctrl>+<C> to create a SIGINT signal, which will
@@ -20,14 +20,15 @@
 _port_remote=22
 _port_local=22
 _port_tunnel=1024
-_host_remote="localhost"
+_host_remote="remote.host"
 _host_local="localhost"
+_date="%A, %Y-%m-%d (%H:%M:%S)"
 _sleep=1m
 
 #
 sigint()
 {
-	echo "Received SIGINT (<Ctrl>+<C>), so we exit here." >&2
+	echo -e "\nReceived SIGINT (<Ctrl>+<C>), so we exit here." >&2
 	exit
 }
 
@@ -36,14 +37,19 @@ trap sigint SIGINT
 #
 _count=1
 echo "Starting SSH reverse tunnel."
+
 while true; do
 
-	ssh -p${_port_remote} -N -R ${_port_tunnel}:${_host_local}:${_port_local} ${_host_remote} #-f
+	date +"${_date}"
+
+	echo
+	ssh -p${_port_remote} -o ExitOnForwardFailure=yes -N -R ${_port_tunnel}:${_host_local}:${_port_local} ${_host_remote} #-f
+	echo
 
 	let _count=$_count+1
 	echo -n "Restarting SSH reverse tunnel #${_count}... "
 
-	if [[ -z $_sleep || $_sleep -eq 0 ]]; then
+	if [[ -z $_sleep ]]; then
 		echo "now."
 	else
 		echo "in ${_sleep}."
